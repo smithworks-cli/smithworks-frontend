7c419674403d48f50ad79342befb8e47a6a24ae7
Zach Bricker <zach@datageek.io>
2020-04-08 22:40:07 -0400

    Almost done with abstractusermodel

---
+++ account/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+import uuid
+
+# Create your models here.
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must provide an email address")
+        if not username:
+            raise ValueError("Users must provide a username")
+
+        user = self.model(email=self.normalize_email(email), username=username,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email), password=password, username=username
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+
+class Account(AbstractBaseUser):
+    """Model definition for Account."""
+
+    # TODO: Define fields here
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name="email", max_length=80, unique=True)
+    username = models.CharField(unique=True, max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+    ]
+
+    objects = CustomAccountManager()
+
+    class Meta:
+        """Meta definition for Account."""
+
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+
+    def __str__(self):
+        """Unicode representation of Account."""
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
